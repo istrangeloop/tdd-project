4f8a17af3da377879f97a11b165d89efd2a91eb4
Ingrid <ingridespangler@gmail.com>
2021-08-12 21:58:07 -0300

    Modelo de usuário mínimo

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+    email = models.EmailField(primary_key=True)
